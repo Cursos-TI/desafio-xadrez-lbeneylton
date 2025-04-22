d7e70e8d57c7307ef2699843a73177f9017fa879
Lucas Beneylton <47756391+lbeneylton@users.noreply.github.com>
2025-04-21 21:11:58 -0300

    Ajustes nos comentarios

--- DesafioMestre/main.c
+++ DesafioMestre/main.c
@@ -11,42 +11,43 @@
 // }
 
 
-//Função para movimento da torre para direita
+//Função para movimento da Torre para direita
 void moveTower(int n){
-    if (n > 0){                       //If para verificar se a recursividade deve continuar
+    if (n > 0){                       //Verifica se a recursividade deve continuar
         printf("Direita\n");            
-        moveTower(n-1);               //Chama a função novamente mais agora com n igual a n-1
+        moveTower(n-1);               //Chama a função novamente, agora com n igual a n-1
     }
 }
 
 
 //Função para o movimento do Bispo na diagonal superior direita
 void moveBishop(int n){
-    if (n > 0){                       //If para verificar se a recursividade deve continuar
+    if (n > 0){                       //Verifica se a recursividade deve continuar
         printf("Cima Direita\n");       
-        moveBishop(n-1);              //Chama a função novamente mais agora com n igual a n-1
+        moveBishop(n-1);              //Chama a função novamente, agora com n igual a n-1
     }
     
 }
 
 
-//Função para movimento da Rainha na diagonal superior direita
+//Função para movimento da Rainha para esquerda
 void moveQueen(int n){
-    if (n > 0){
+    if (n > 0){                  //Verifica se a recursividade deve continuar
         printf("Esquerda\n");
-        moveQueen(n-1);
+        moveQueen(n-1);          //Chama a função novamente, agora com n igual a n-1
     }
+    
 }
 
 
 //Função para movimento do Cavalo em L para baixo e esquerda
 void moveHorse(int n){
-    if (n > 0){
-        for(int i=0;i<2;i++){
+    if (n > 0){                   //Verifica se a recursividade deve continuar
+        for(int i=0;i<2;i++){     //Move o cavalo 2 vezes para baixo
             printf("Baixo\n");
         }
         printf("Esquerda\n");        
-        moveHorse(n-1);
+        moveHorse(n-1);           //Chama a função novamente, agora com n igual a n-1
     }
 }
 
@@ -58,18 +59,22 @@ int main(){
     int queen = 8;
     int bishop = 5;
 
+    printf("\n-------------------------\n");
     printf("A torre irá se movimentar:\n");
     printf("\n\n");
     moveTower(tower);
 
+    printf("\n-------------------------\n");
     printf("O bispo irá se movimentar:\n");
     printf("\n\n");
     moveBishop(bishop);
 
+    printf("\n-------------------------\n");
     printf("A rainha irá se movimentar:\n");
     printf("\n\n");
     moveQueen(queen);
 
+    printf("\n-------------------------\n");
     printf("O cavalo irá se movimentar:\n");
     printf("\n\n");
     moveHorse(horse);
